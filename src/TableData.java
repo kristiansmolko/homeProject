70c548d1f776cc2bad60722e1e159d23b8f0993c
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-12-30 16:53:59 +0100

    Added tableData for table

---
+++ src/TableData.java
@@ -0,0 +1,97 @@
+import javafx.beans.property.SimpleStringProperty;
+
+public class TableData {
+    private String first;
+    private String second;
+    private String third;
+    private String fourth;
+    private String fifth;
+    private String sixth;
+    private String seventh;
+    private String eight;
+    private String ninth;
+
+    public TableData(String first, String second, String third, String fourth, String fifth, String sixth, String seventh, String eight, String ninth) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+        this.fourth = fourth;
+        this.fifth = fifth;
+        this.sixth = sixth;
+        this.seventh = seventh;
+        this.eight = eight;
+        this.ninth = ninth;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+
+    public String getThird() {
+        return third;
+    }
+
+    public void setThird(String third) {
+        this.third = third;
+    }
+
+    public String getFourth() {
+        return fourth;
+    }
+
+    public void setFourth(String fourth) {
+        this.fourth = fourth;
+    }
+
+    public String getFifth() {
+        return fifth;
+    }
+
+    public void setFifth(String fifth) {
+        this.fifth = fifth;
+    }
+
+    public String getSixth() {
+        return sixth;
+    }
+
+    public void setSixth(String sixth) {
+        this.sixth = sixth;
+    }
+
+    public String getSeventh() {
+        return seventh;
+    }
+
+    public void setSeventh(String seventh) {
+        this.seventh = seventh;
+    }
+
+    public String getEight() {
+        return eight;
+    }
+
+    public void setEight(String eight) {
+        this.eight = eight;
+    }
+
+    public String getNinth() {
+        return ninth;
+    }
+
+    public void setNinth(String ninth) {
+        this.ninth = ninth;
+    }
+}
