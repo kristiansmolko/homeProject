4c03c36e9c5ac1c1190199cde4542df11e15c93f
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-12-31 13:01:13 +0100

    Deleted addToShip

--- src/Ship.java
+++ src/Ship.java
@@ -1,21 +1,12 @@
 import java.util.ArrayList;
 
 public class Ship {
-    private ArrayList<Ship> list;
     private int row;
     private int col;
 
     public Ship(int row, int col){
         this.row = row;
         this.col = col;
-    }
-
-    public void addToShips(Ship ship){
-        list.add(ship);
-    }
-
-    public ArrayList<Ship> getList(){
-        return list;
     }
 
     public int getRow() {
