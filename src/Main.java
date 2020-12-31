0a6eb64180f307b31cedfafe9e44f8be0387300a
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-12-31 16:51:05 +0100

    Modified placing, added data for another table where player tries to find ships

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,18 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
        BorderPane root = new BorderPane();
        TableView<TableData> table = createSetupForPlayer();
+       TableView<TableData> tableStart = createTable();
+       Button newScene = new Button("Start");
+       newScene.setOnAction(actionEvent -> {
+           BorderPane root1 = new BorderPane();
+           root1.setCenter(tableStart);
+           Scene scene2 = new Scene(root1, 200, 250);
+           stage.setScene(scene2);
+           stage.show();
+       });
        root.setCenter(table);
-       Scene scene = new Scene(root, 200, 241);
+       root.setBottom(newScene);
+       Scene scene = new Scene(root, 200, 265);
        stage.setScene(scene);
        stage.show();
     }
@@ -655,17 +665,15 @@ public class Main extends Application {
                     field[row-2][col] = 0;
                 }
             }
-
             if (field[row][col+1] == 1){
                 field[row][col-1] = 0;
                 field[row][col+2] = 0;
             }
-            else if (field[row][col-1] == 1){
+            if (field[row][col-1] == 1){
                 field[row][col+1] = 0;
-                field[row][col-2] = field[row][col-2]==-1?-1:0;
+                field[row][col-2] = 0;
             }
-            else if (field[row-1][col] == 1)
-                field[row-2][col] = 0;
+            if (field[row-1][col] == 1) field[row-2][col] = 0;
         }
 
         //first col
@@ -746,15 +754,39 @@ public class Main extends Application {
         }
 
         setup();
-        if ((col < 6) && (field[row][col+3] == 1)) field[row][col+1] = -1;
-        if ((col < 5) && (field[row][col+4] == 1)) field[row][col+2] = -1;
-        if ((col > 2) && (field[row][col-3] == 1)) field[row][col-1] = -1;
-        if ((col > 3) && (field[row][col-4] == 1)) field[row][col-2] = -1;
+        //col
+        if ((col < 6) && (field[row][col+3] == 1)) field[row][col+1] = field[row][col+1]==1?1:-1;
+        if ((col < 6) && (row > 2) && ((field[row-2][col+3] == 1) || (field[row-1][col+3] == 1))) field[row][col+1] = field[row][col+1]==1?1:-1;
+        if ((col < 6) && (row < 7) && ((field[row+2][col+3] == 1) || (field[row+1][col+3] == 1))) field[row][col+1] = field[row][col+1]==1?1:-1;
 
-        if ((row < 6) && (field[row+3][col] == 1)) field[row+1][col] = -1;
-        if ((row < 5) && (field[row+4][col] == 1)) field[row+2][col] = -1;
-        if ((row > 2) && (field[row-3][col] == 1)) field[row-1][col] = -1;
-        if ((row > 3) && (field[row-4][col] == 1)) field[row-2][col] = -1;
+        if ((col < 5) && (field[row][col+4] == 1)) field[row][col+2] = field[row][col+2]==1?1:-1;
+        if ((col < 5) && (row > 2) && ((field[row-2][col+4] == 1) || (field[row-1][col+4] == 1))) field[row][col+2] = field[row][col+2]==1?1:-1;
+        if ((col < 5) && (row < 7) && ((field[row+1][col+4] == 1) || (field[row+2][col+4] == 1))) field[row][col+2] = field[row][col+2]==1?1:-1;
+
+        if ((col > 2) && (field[row][col-3] == 1)) field[row][col-1] = field[row][col-1]==1?1:-1;
+        if ((col > 2) && (row > 2) && ((field[row-2][col-3] == 1) || (field[row-1][col-3] == 1))) field[row][col-1] = field[row][col-1]==1?1:-1;
+        if ((col > 2) && (row < 7) && ((field[row+2][col-3] == 1) || (field[row+1][col-3] == 1))) field[row][col-1] = field[row][col-1]==1?1:-1;
+
+        if ((col > 3) && (field[row][col-4] == 1)) field[row][col-2] = field[row][col-2]==1?1:-1;
+        if ((col > 3) && (row > 2) && ((field[row-2][col-4] == 1) || (field[row-1][col-4] == 1))) field[row][col-2] = field[row][col-2]==1?1:-1;
+        if ((col > 3) && (row < 7) && ((field[row+2][col-4] == 1) || (field[row+1][col-4] == 1))) field[row][col-2] = field[row][col-2]==1?1:-1;
+
+        //rows
+        if ((row < 6) && (field[row+3][col] == 1)) field[row+1][col] = field[row+1][col]==1?1:-1;
+        if ((row < 6) && (col > 2) && ((field[row+3][col-2] == 1) || (field[row+3][col-1] == 1))) field[row+1][col] = field[row+1][col]==1?1:-1;
+        if ((row < 6) && (col < 7) && ((field[row+3][col+1] == 1) || (field[row+3][col+2] == 1))) field[row+1][col] = field[row+1][col]==1?1:-1;
+
+        if ((row < 5) && (field[row+4][col] == 1)) field[row+2][col] = field[row+2][col]==1?1:-1;
+        if ((row < 5) && (col > 2) && ((field[row+4][col-2] == 1) || (field[row+4][col-1] == 1))) field[row+2][col] = field[row+2][col]==1?1:-1;
+        if ((row < 5) && (col < 7) && ((field[row+4][col+1] == 1) || (field[row+4][col+2] == 1))) field[row+2][col] = field[row+2][col]==1?1:-1;
+
+        if ((row > 2) && (field[row-3][col] == 1)) field[row-1][col] = field[row-1][col]==1?1:-1;
+        if ((row > 2) && (col > 2) && ((field[row-3][col-2] == 1) || (field[row-3][col-1] == 1))) field[row-1][col] = field[row-1][col]==1?1:-1;
+        if ((row > 2) && (col < 7) && ((field[row-3][col+1] == 1) || (field[row-3][col+2] == 1))) field[row-1][col] = field[row-1][col]==1?1:-1;
+
+        if ((row > 3) && (field[row-4][col] == 1)) field[row-2][col] = field[row-2][col]==1?1:-1;
+        if ((row > 3) && (col > 2) && ((field[row-4][col-2] == 1) || (field[row-4][col-1] == 1))) field[row-2][col] = field[row-2][col]==1?1:-1;
+        if ((row > 3) && (col < 7) && ((field[row-4][col+1] == 1) || (field[row-4][col+2] == 1))) field[row-2][col] = field[row-2][col]==1?1:-1;
     }
 
     private static void setup(){
@@ -763,26 +795,40 @@ public class Main extends Application {
        }
     }
 
-    private ObservableList<TableData> getData(){
+    private ObservableList<TableData> getDataForSetup(){
         ObservableList<TableData> data = FXCollections.observableArrayList();
-        data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
-        data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
-        data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
-        data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
-        data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
-        data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
-        data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
-        data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
-        data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
+        data.add(new TableData(field[0][0], field[0][1], field[0][2], field[0][3], field[0][4], field[0][5], field[0][6], field[0][7], field[0][8]));
+        data.add(new TableData(field[1][0], field[1][1], field[1][2], field[1][3], field[1][4], field[1][5], field[1][6], field[1][7], field[1][8]));
+        data.add(new TableData(field[2][0], field[2][1], field[2][2], field[2][3], field[2][4], field[2][5], field[2][6], field[2][7], field[2][8]));
+        data.add(new TableData(field[3][0], field[3][1], field[3][2], field[3][3], field[3][4], field[3][5], field[3][6], field[3][7], field[3][8]));
+        data.add(new TableData(field[4][0], field[4][1], field[4][2], field[4][3], field[4][4], field[4][5], field[4][6], field[4][7], field[4][8]));
+        data.add(new TableData(field[5][0], field[5][1], field[5][2], field[5][3], field[5][4], field[5][5], field[5][6], field[5][7], field[5][8]));
+        data.add(new TableData(field[6][0], field[6][1], field[6][2], field[6][3], field[6][4], field[6][5], field[6][6], field[6][7], field[6][8]));
+        data.add(new TableData(field[7][0], field[7][1], field[7][2], field[7][3], field[7][4], field[7][5], field[7][6], field[7][7], field[7][8]));
+        data.add(new TableData(field[8][0], field[8][1], field[8][2], field[8][3], field[8][4], field[8][5], field[8][6], field[8][7], field[8][8]));
         return data;
     }
 
-    private TableView createTable(){
+    private ObservableList<TableData> getData(){
+       ObservableList<TableData> data = FXCollections.observableArrayList();
+       data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
+       data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
+       data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
+       data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
+       data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
+       data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
+       data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
+       data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
+       data.add(new TableData("X", "X", "X", "X", "X", "X", "X", "X", "X"));
+       return data;
+    }
+
+    private TableView<TableData> createTable(){
         TableView<TableData> table = new TableView<>();
         table.setEditable(false);
         TextField text = new TextField();
         BorderPane root = new BorderPane();
-        root.setTop(new Label("Setup"));
+        root.setTop(new Label("Game"));
         root.setBottom(text);
 
         TableColumn<TableData, String> firstCol = createCol("1", "first");
@@ -877,10 +923,6 @@ public class Main extends Application {
     private TableView<TableData> createSetupForPlayer(){
         TableView<TableData> table = new TableView<>();
         table.setEditable(false);
-        TextField text = new TextField();
-        BorderPane root = new BorderPane();
-        root.setTop(new Label("Setup"));
-        root.setBottom(text);
 
         TableColumn<TableData, String> firstCol = createCol("1", "one");
         TableColumn<TableData, String> secondCol = createCol("2", "two");
@@ -892,7 +934,7 @@ public class Main extends Application {
         TableColumn<TableData, String> eightCol = createCol("8", "eight");
         TableColumn<TableData, String> ninthCol = createCol("9", "nine");
 
-        table.setItems(getData());
+        table.setItems(getDataForSetup());
         table.getColumns().addAll(firstCol, secondCol, thirdCol, fourthCol, fifthCol, sixthCol, seventhCol, eightCol, ninthCol);
         table.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         table.getSelectionModel().setCellSelectionEnabled(true);
@@ -902,19 +944,14 @@ public class Main extends Application {
             int col = tablePosition.getColumn();
             addToField(row, col);
 
-            table.setItems(getData());
+            table.setItems(getDataForSetup());
             table.refresh();
         });
        return table;
     }
 
     public static void main(String[] args) {
-        addToField("i1");
-        addToField("b1");
-        addToField("a2");
-        addToField("a1");
-        //printField();
-        launch(args);
+       launch(args);
     }
 
 }
