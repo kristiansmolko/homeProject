7b7407bdc7dde426e61e7c4637fe26ee0b82e622
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-01-02 14:35:43 +0100

    Created class player

---
+++ src/Player.java
@@ -0,0 +1,31 @@
+public class Player {
+    private int turn;
+    private int[][] field;
+    private int count;
+
+    public Player(int turn){
+        this.turn = turn;
+        count = 0;
+        field = new int[][] {{0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}};
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public int[][] getField() {
+        return field;
+    }
+
+    public void setField(int[][] field) {
+        this.field = field;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
